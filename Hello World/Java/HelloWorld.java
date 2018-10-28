b56e83d844ace828177f0655d1e1896cdf1a9494
charith14 <38108659+charith14@users.noreply.github.com>
2018-10-28 20:40:53 +0530

    Update HelloWorld.java

--- Hello World/Java/HelloWorld.java
+++ Hello World/Java/HelloWorld.java
@@ -4,3 +4,13 @@ class HelloWorld
                 System.out.println("Hello World!!");
         }
 }
+
+//my change
+
+class HelloWorld
+{
+        public static void main(String[] args)
+        {
+                printf("Hello World!!");
+        }
+}
